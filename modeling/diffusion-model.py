5a260964adc5d17d79b4763635f7e2332733c8e9
MeganKai <megankairiss2@gmail.com>
2019-04-01 15:39:46 -0400

    updating modeling code

--- modeling/diffusion-model.py
+++ modeling/diffusion-model.py
@@ -2,12 +2,14 @@ import pandas as pd
 import hddm
 import matplotlib.pyplot as plt
 
-def main():
-    path = "/home/megan/Desktop/Winter\ 2019/Diffusion-Models/"
- 
-    data = hddm.load_csv("diffusion-feb.csv")
-    print(data.head(10))
-    data = hddm.utils.flip_errors(data)
+# Simple model build; using gradient ascent optimization
+# Draws posterior samples
+def simpleModel(data):
+    
+    m = hddm.HDDM(data,p_outlier=0.05)
+    m.find_starting_values()
+    m.sample(20,burn=1)
+
 
     fig = plt.figure()
     ax = fig.add_subplot(111,xlabel='RT',ylabel='count',title='RT Distributions')
@@ -15,16 +17,19 @@ def main():
         subj_data.rt.hist(bins=20,histtype='step',ax=ax)
     plt.savefig('hddm_demo_fig.pdf')
     models = []
-    #for i in range(5):
-    m = hddm.HDDM(data,p_outlier=0.05)
-    m.find_starting_values()
-    m.sample(200,burn=2)
 
     stats = m.gen_stats()
     stats[stats.index.isin(['a','a_std','a_subj.0','a_subj.1'])]
     m.plot_posteriors(['a','t','v','a_std'])
-    #m.savefig('posteriors.pdf')
+    plt.savefig("test.pdf")
 
+def main():
+
+    path = "/home/megan/Desktop/Drift-Diffusion/data"
+ 
+    data = hddm.load_csv("/home/megan/Desktop/Drift-Diffusion/data/diffusion-feb.csv")
+
+    simpleModel(data)
 
 
 main()
